6ec17d2a541b7cf8054529e4f926780214248581
Cons Bulaqueña <consbulaquena@gmail.com>
2019-02-05 00:48:40 +0800

    Add functions

---
+++ functions/functions101.js
@@ -0,0 +1,32 @@
+//Functions are sub program you can run again and again.
+// Function - input (argument), code, output (return value)
+
+let greetUser = function () {
+    console.log('Welcome, admin!')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+//End
+//Square Function
+
+let square = function (num) {
+    let result = num * num
+    return result
+}
+
+let value = square(3)
+//single argument passed to the function
+let otherValue = square(10)
+
+
+console.log(value)
+console.log(otherValue)
+
+//Challenge Area
+
+
+
+
