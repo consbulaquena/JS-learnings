01c8aaba8018f723316cf3c0e1ee5c1817b58eff
Cons Bulaqueña <consbulaquena@gmail.com>
2019-02-09 23:50:58 +0800

    Using default arguments

--- functions/arguments.js
+++ functions/arguments.js
@@ -1,10 +1,22 @@
 //multiple arguments in function
 //a, b, c
+// Add a+b
 
-let add = function (a, b) {
-    return a + b
+let add = function (a, b, c) {
+    return a + b + c
 }
 
 //pass in value 10 for a, 1 for b
-let result = add(10, 1)
+let result = add(10, 1, 5)
 console.log(result)
+
+// How to use default arguments
+// Game leaderboard most points to least
+
+let getScoreText = function (name, score = 10) {
+    console.log(name)
+    console.log(score)
+}
+
+getScoreText()
+
