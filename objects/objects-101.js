2e70ebc2c4119ea7e45daf05a4e454398ad37f4b
Cons Bulaqueña <consbulaquena@gmail.com>
2019-03-12 16:44:07 +0800

    Add 'MyPerson' Object as challenge

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -1,7 +1,30 @@
+// Model of a Book
+
 let myBook = {
     title: 'Total Money Makeover',
     author: 'Dave Ramsey',
     pageCount: 256
 }
 
-console.log(myBook.title)
+console.log(`${myBook.title} by ${myBook.author}`)
+
+//Change property of title object with dot notation
+//the title of the book changed
+myBook.title = 'Financial Peace University'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+
+
+//Challenge Area Model of a Person
+// name, age, location
+// Lindsay, 58, Watercolor artist
+
+let myPerson = {
+    name: 'Lindsay',
+    age: '58',
+    job: 'Watercolor artist'
+}
+
+console.log(`${myPerson.name} is ${myPerson.age} years old and works as a ${myPerson.job}`)
+
