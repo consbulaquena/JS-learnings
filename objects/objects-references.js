ff647f242af7466c5014f8f09535b3ce2aa0b7bc
Cons Bulaqueña <consbulaquena@gmail.com>
2019-03-15 22:25:03 +0800

    Try out challenge expense tracker

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,6 +1,21 @@
 //expense tracker challenge
 
 
+
+//1 add income take acct to manipulate, income for, take income try add
+//2 reset account reset expenses/income to zero
+//3 Get account summary - act bounce total expense, income -
+// Account for Cons has 900 dollars , 1000 in income, 100 in expenses
+
+//use functions
+
+//addincome
+// addExpense
+// getAccountSummary
+// resetAccount
+// getAccountSummary
+
+
 let myAccount = {
     name: 'Cons Bulaquena',
     expenses: 0,
@@ -16,17 +31,32 @@ let addExpense = function(account, amount) {
 addExpense(myAccount, 2.50)
 console.log(myAccount)
 
-//1 add income take acct to manipulate, income for, take income try add
-//2 reset account reset expenses/income to zero
-//3 Get account summary - act bounce total expense, income -
-// Account for Cons has 900 dollars , 1000 in income, 100 in expenses
 
-//use functions
 
-//addincome
-// addExpense
+let addIncome= function(account, amount) {
+    account.income = account.income + amount
+    console.log(account)
+}
+
+
+addIncome(myAccount, 2.50)
+console.log(myAccount)
+
 // getAccountSummary
+
+let getSummary = function (account) {
+    return {
+        summary: `${account.name} income is ${account.income} expenses ${account.expenses}`,
+    
+    }
+}
+
+let accountSummary = getSummary(myAccount)
+
+
+console.log(myAccount.summary)
+
+
 // resetAccount
 // getAccountSummary
-
 
