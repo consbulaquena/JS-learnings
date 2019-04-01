0b0aa2fd9c268e305e1008fd47bf3eaf157d9d59
Cons Bulaqueña <consbulaquena@gmail.com>
2019-04-02 00:03:05 +0800

    add methods

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -1,14 +1,18 @@
-let name = 'Cons'
+let name = 'Cons Bulaquena'
 
 //Length property - there are 14 characters
 
 console.log(name.length)
 
-// Can validate number of password
+// Can validate number of passwords
 //convert to upper case
 //this 1 method
 console.log(name.toUpperCase())
 
+console.log(name.toLowerCase())
+
+let password = 'g0ldrIdden'
+console.log(password.includes('password'))
 
 
-
+console.log(name.trim())
