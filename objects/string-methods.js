200110f7990a9e7cac42c128154fa552350c7802
Cons Bulaqueña <consbulaquena@gmail.com>
2019-04-04 17:01:08 +0800

    Add isValidPassword function -string methods

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -1,14 +1,45 @@
+//string is the name
 let name = 'Cons Bulaquena'
 
 //Length property - there are 14 characters
 
 console.log(name.length)
 
-// Can validate number of password
+// Can validate number of passwords
 //convert to upper case
-//this i method
+//Using method <toUppercase()>
+
 console.log(name.toUpperCase())
 
+console.log(name.toLowerCase())
 
+//.includes() method
+//strong password by not having direct 1234 password
+let password = 'g0ldrIdden1234'
+console.log(password.includes('1234'))
+
+
+console.log(name.trim())
+
+
+//Challenge area
+//is valid password
+
+let isValidPassword = function (password) {
+    if (password.length > 8 && !password.includes('password')) {
+        return true
+} else {
+        return false
+    }
+}
+
+console.log(isValidPassword('asfw'))
+
+console.log(isValidPassword('asfwM4n1f1c3nt'))
+
+console.log(isValidPassword('12quallpassword'))
+
+
+//length is more than 8, and doesnt contain the word abcd
 
 
