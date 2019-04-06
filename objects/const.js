d75bcdbba16544a2ba7bad18aa54665853a728c3
Cons Bulaqueña <consbulaquena@gmail.com>
2019-04-06 13:12:45 +0800

    Add const

---
+++ objects/const.js
@@ -0,0 +1,8 @@
+//alternative to "let", we use const
+// Const is used, if we do not Re-Assign. Used Once only
+
+const isRaining = true
+
+//isRaining = false         Const is Used Once
+
+console.log(isRaining)
