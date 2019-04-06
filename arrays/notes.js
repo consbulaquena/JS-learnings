4eca82315c05b2953386cbdb3c8a2813b39a5c49
Cons Bulaqueña <consbulaquena@gmail.com>
2019-04-06 14:19:30 +0800

    Add arrays

---
+++ arrays/notes.js
@@ -0,0 +1,7 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+console.log(notes.length)
+console.log(notes[2])
+console.log(notes[notes.length - 1])
+// we use -1 to always access the last number 3
+ 
---
+++ arrays/todo.js
@@ -0,0 +1,7 @@
+//array with 5 todos
+
+const todos = ['Todo 1', 'Todo 2', 'Todo 3', 'Save $1000', 'Invest in stocks']
+
+console.log(`You have ${todos.length} to-dos`)
+console.log(`To-dos: ${todos[3]}`)
+console.log(`To-dos: ${todos[4]}`)
