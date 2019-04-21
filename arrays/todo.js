9d5a6c7ca34122900c66e7239dda40f577eef136
Cons Bulaqueña <consbulaquena@gmail.com>
2019-04-21 20:56:27 +0800

    Add methods in array

--- arrays/todo.js
+++ arrays/todo.js
@@ -2,6 +2,17 @@
 
 const todos = ['Todo 1', 'Todo 2', 'Todo 3', 'Save $1000', 'Invest in stocks']
 
+console.log(todos.pop())
+todos.pop()
+todos.push('My new note')
+
+console.log(todos.length)
+console.log(todos)
+
 console.log(`You have ${todos.length} to-dos`)
 console.log(`To-dos: ${todos[3]}`)
 console.log(`To-dos: ${todos[4]}`)
+
+
+
+
