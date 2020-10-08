607905f537646d361fd8856c4bfbe2c0096b8a61
Cons Bulaquena <consbulaquena@gmail.com>
2020-10-08 14:29:46 +0800

    Add Script.js and index.html

---
+++ JS-basics-2020/script.js
@@ -0,0 +1,233 @@
+/*
+* comment here
+*/
+
+
+var firstName = 'Consor';
+console.log(firstName);
+
+//console.log('yoo');
+
+
+//result John 28
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+//multiple variables then assign values
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+
+console.log(firstName + ' is a ' + age + job + '. Is he married? '+ isMarried);
+
+//variable mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + job + '. Is he married? '+ isMarried);
+
+//prompt input last name
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+
+var year = 2020;
+var yearLucas = year - 22;
+
+console.log(yearLucas);
+
+//type of operator
+var ageC = 22;
+console.log(typeof ageC)
+
+// If else statements
+
+var firstName = 'Lucas';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is marrid! ');
+} else {
+    console.log(firstName + ' will hopefully marry soon!');
+}
+
+
+
+var isMarried = true;
+
+if (isMarried) {
+    console.log(firstName + ' is married! ');
+} else {
+    console.log(firstName + ' will hopefully marry soon!');
+}
+
+/*
+*Boolean Logic
+*/
+//else if is for multiple ifs
+
+var firstName = 'Angel';
+var age = 16;
+
+if (age < 13) {
+    console.log(firstName + ' is a girl ');
+} else if (age >= 13 && age < 20) {//between 13 and 20, true and true makes true
+    console.log(firstName + ' is a teenager. ');
+
+} else {
+    console.log(firstName + ' is a woman ');
+}
+
+
+// Ternary Operator and Switch Statements
+
+var firstname = 'Cons';
+var age = 16;
+
+// ? is if block, else block is colon:
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.')
+
+
+
+// Switch statement
+// use case if more than 4 else if
+
+var job = 'teacher';
+switch (job) {
+    case 'teacher': //if
+        console.log(firstName + ' teaches kid how to code ');
+        break;
+    case 'driver': //else
+        console.log(firstName + ' teaches kid how to drive ');
+    break;
+    case 'maid': //else if
+    console.log(firstName + ' teaches kid how to clean ');
+    break;
+    default:
+        console.log(firstName + ' does something else ');
+
+}
+
+
+// Truthy and falsy values
+
+// height is not defined so if False so printed False in else
+var height;
+
+heighr = 0;
+
+if (height || height === 0) {
+    console.log('Variable defined');
+} else {
+    console.log('variable has NOT defined')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
